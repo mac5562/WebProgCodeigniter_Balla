e822d71729dfb88ff0c6c88cd163f0325dd15adb
Lovász Ákos <akos4x44@gmail.com>
2020-03-02 14:32:05 +0100

    Commit the whole fucking thing
    because this is how you should use git lmao

---
+++ application/controllers/Employees.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Employees
+ *
+ * @author akos4
+ * 
+ * adatbázisban hoz létre egy táblát amiben adatokat tárolunk majd.
+ * a kontroller a kezelő funkciókat biztosít böngészőn keresztül
+ */
+class Employees {
+    
+    public function list(){
+        echo "list";
+    }
+    
+    public function add(){
+        echo "add";
+    }
+    
+    public function edit(){
+        echo "edit";
+    }
+    
+    public function delete(){
+        echo "delete";
+    }
+}
